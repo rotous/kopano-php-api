aa7846b274b3af4bf386fba0defdfe900a2a5061
Ronald Toussaint <ronald@dekolos.com>
2017-08-19 14:19:49 +0200

    [Sami - phpduck theme]: Made left and rigt column resizable by dragging

--- sami-themes/phpduck/js/phpduck.js
+++ sami-themes/phpduck/js/phpduck.js
@@ -4,6 +4,8 @@
     function initializePhpDuck(){
         window.addEventListener('resize', resizeClassNavSubmenus);
         resizeClassNavSubmenus();
+
+        initializeDragHandle();
     }
 
     function resizeClassNavSubmenus(){
@@ -20,5 +22,26 @@
                 ul.style.width = w + 'px';
             });
         }
+    }
+
+    // TODO store the state in the localStorage
+    function initializeDragHandle(){
+        let handle = document.querySelector('#drag-handle');
+        let left = document.querySelector('#left-column');
+        let right = document.querySelector('#right-column');
+        handle.addEventListener("dragstart", function( event ) {
+            // make it half transparent
+            handle.style.opacity = .5;
+            handle.style.background = '#ccc';
+        }, false);
+        handle.addEventListener("dragend", function( event ) {
+            let x = event.clientX;
+            left.style.width = x + 'px';
+            right.style.width = 'calc(100% - ' + x + 'px)';
+            right.style.marginLeft = x + 'px';
+            // remove transparency
+            handle.style.opacity = 1;
+            handle.style.background = 'transparent';
+        }, false);
     }
 })();
