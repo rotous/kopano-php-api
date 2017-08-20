09b32ae1115746ac9cac7290d60174178d90f529
Ronald Toussaint <ronald@dekolos.com>
2017-08-20 22:35:35 +0200

    [Sami - phpduck theme]: Added column width storing in localStorage [Sami - phpduck theme]: Added hide on click for menu

--- sami-themes/phpduck/js/phpduck.js
+++ sami-themes/phpduck/js/phpduck.js
@@ -1,4 +1,10 @@
 (function(){
+    // Read the column width from localStorage
+    let w = parseInt(localStorage.getItem('column-left-width'), 10);
+    if ( w ){
+        setLeftColumnWidth(w);
+    }
+
     window.addEventListener('load', initializePhpDuck);
 
     function initializePhpDuck(){
@@ -6,6 +12,8 @@
         resizeClassNavSubmenus();
 
         initializeDragHandle();
+
+        initializeMenuBar();
     }
 
     function resizeClassNavSubmenus(){
@@ -29,19 +37,50 @@
         let handle = document.querySelector('#drag-handle');
         let left = document.querySelector('#left-column');
         let right = document.querySelector('#right-column');
+
         handle.addEventListener("dragstart", function( event ) {
             // make it half transparent
             handle.style.opacity = .5;
             handle.style.background = '#ccc';
         }, false);
         handle.addEventListener("dragend", function( event ) {
-            let x = event.clientX;
-            left.style.width = x + 'px';
-            right.style.width = 'calc(100% - ' + x + 'px)';
-            right.style.marginLeft = x + 'px';
+            let w = Math.max(event.clientX, 50);
+            setLeftColumnWidth(w);
+            // Store in localstorage
+            localStorage.setItem('column-left-width', w);
             // remove transparency
             handle.style.opacity = 1;
             handle.style.background = 'transparent';
         }, false);
+    }
+
+    function setLeftColumnWidth(w){
+        let left = document.querySelector('#left-column');
+        let right = document.querySelector('#right-column');
+        w = Math.max( w, 50);
+        w = Math.min(w, window.innerWidth - 50);
+        left.style.width = w + 'px';
+        right.style.width = 'calc(100% - ' + w + 'px)';
+        right.style.marginLeft = w + 'px';
+    }
+
+    function initializeMenuBar(){
+        let menuItems = document.querySelectorAll('.class-navigation-menu-header');
+        for ( let i=0; i<menuItems.length; i++ ){
+            menuItems[i].addEventListener('mouseover', function(e){
+                menuItems[i].classList.add('pd-mouseover');
+            });
+            menuItems[i].addEventListener('mouseout', function(e){
+                menuItems[i].classList.remove('pd-mouseover');
+            });
+        }
+
+        let items = document.querySelectorAll('.class-nav-submenu a');
+        for ( let i=0; i<items.length; i++ ){
+            items[i].addEventListener('click', function(e){
+                let menu = e.target.parentElement.parentElement.parentElement;
+                menu.classList.remove('pd-mouseover');
+            });
+        }
     }
 })();
